b9fc9298d8759a8590ecb29bd4eeb4b17b23e1dd
Mateusz Jablonski <mjablonski.praca@gmail.com>
2023-02-14 15:42:30 +0100

    slider ready on default style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// import Splide from '@splidejs/splide';
+
+// new Splide('.splide').mount();
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let splide = new Splide('.splide');
+//     splide.mount();
+// })
