1d5b84eb4f57406cf1758e8ee462b3ac6ed01277
Mateusz Jablonski <mjablonski.praca@gmail.com>
2023-02-15 18:55:14 +0100

    finishing responsiveness

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// import Splide from '@splidejs/splide';
-
-// new Splide('.splide').mount();
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     let splide = new Splide('.splide');
-//     splide.mount();
-// })
